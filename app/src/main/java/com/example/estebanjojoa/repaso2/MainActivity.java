7a22313180da1a3bbb1097c330af52b2bf4a9035
Esteban_Dario_Jojoa_Rosero <estebandariojojoa9@gmail.com>
2018-03-19 21:06:18 -0500

    Initial commit

---
+++ app/src/main/java/com/example/estebanjojoa/repaso2/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.estebanjojoa.repaso2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_options, menu);
+        return true;
+    }
+}
